9752003fd3c38204dafb1fb8a26e83af466b3912
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2023-08-10 17:04:55 +0200

    🎨 formated and sytilezed code with sqlstuff

--- models/report_overview.sql
+++ models/report_overview.sql
@@ -1,22 +1,39 @@
-{{ config(materialized='view') }}
-
 {% set campaign_id = 66 %}
 
-
--- CREATE OR REPLACE VIEW campaign_metrics AS
-SELECT
+select
     campaign_id,
-    COUNT(*) FILTER (WHERE status IN ('end installation', 'legal registration', 'legalization', 'warranty payment')) AS total_executed,
-    COUNT(*) FILTER (WHERE status = 'discarded') AS discarded,
-    COUNT(*) AS inscriptions,
-    COUNT(*) FILTER (WHERE status = 'discarded' AND discarded_type = 'technical') AS discarded_technical,
-    COUNT(*) FILTER (WHERE status = 'discarded' AND discarded_type = 'voluntary') AS discarded_voluntary,
-    COUNT(*) FILTER (WHERE status = 'discarded' AND discarded_type = 'voluntary' AND date_report IS NULL) AS discarded_voluntary_before_technical_visit,
-    COUNT(*) FILTER (WHERE status = 'discarded' AND discarded_type = 'voluntary' AND date_report IS NOT NULL) AS discarded_voluntary_after_technical_visit,
-    COUNT(*) FILTER (WHERE status = 'registered') AS pending_prereport,
-    COUNT(*) FILTER (WHERE status = 'prereport') AS pending_technical_visit,
-    COUNT(*) FILTER (WHERE status = 'technical visit') AS pending_technical_scheduled_visit,
-    COUNT(*) FILTER (WHERE status IN (
+    count(*) filter (
+        where status in (
+            'end installation',
+            'legal registration',
+            'legalization',
+            'warranty payment'
+        )
+    ) as total_executed,
+    count(*) filter (where status = 'discarded') as discarded,
+    count(*) as inscriptions,
+    count(*) filter (
+        where status = 'discarded' and discarded_type = 'technical'
+    ) as discarded_technical,
+    count(*) filter (
+        where status = 'discarded' and discarded_type = 'voluntary'
+    ) as discarded_voluntary,
+    count(*) filter (
+        where status = 'discarded'
+        and discarded_type = 'voluntary'
+        and date_report is null
+    ) as discarded_voluntary_before_technical_visit,
+    count(*) filter (
+        where status = 'discarded'
+        and discarded_type = 'voluntary'
+        and date_report is not null
+    ) as discarded_voluntary_after_technical_visit,
+    count(*) filter (where status = 'registered') as pending_prereport,
+    count(*) filter (where status = 'prereport') as pending_technical_visit,
+    count(*) filter (
+        where status = 'technical visit'
+    ) as pending_technical_scheduled_visit,
+    count(*) filter (where status in (
         'report', 'report review',
         'offer', 'offer review',
         'signature',
@@ -26,10 +43,15 @@ SELECT
         'legal registration',
         'legalization',
         'warranty payment'
-    )) + COUNT(*) FILTER (WHERE status = 'discarded' AND discarded_type = 'technical') AS total_technical_visits_done,
-    COUNT(*) FILTER (WHERE status IN ('report', 'report review')) AS technical_visit_report,
-    COUNT(*) FILTER (WHERE status IN ('offer', 'offer review')) AS offers_sent,
-    COUNT(*) FILTER (WHERE status IN (
+    ))
+    + count(*) filter (
+        where status = 'discarded' and discarded_type = 'technical'
+    ) as total_technical_visits_done,
+    count(*) filter (
+        where status in ('report', 'report review')
+    ) as technical_visit_report,
+    count(*) filter (where status in ('offer', 'offer review')) as offers_sent,
+    count(*) filter (where status in (
         'signature',
         'construction',
         'date installation set',
@@ -37,38 +59,60 @@ SELECT
         'legal registration',
         'legalization',
         'warranty payment'
-    )) AS total_signed_contracts,
-    COUNT(*) FILTER (WHERE status = 'signature') AS signed_contracts,
-    COUNT(*) FILTER (WHERE status = 'construction permit') AS pending_license_response,
-    COUNT(*) FILTER (WHERE status = 'date installation set') AS scheduled_works,
-    COUNT(*) FILTER (WHERE status = 'end installation') AS works_completed,
-    COUNT(*) FILTER (WHERE status = 'legal registration') AS legalization_stage,
-    COUNT(*) FILTER (WHERE status = 'legalization') AS legalization_completed,
-    COUNT(*) FILTER (WHERE status IN (
+    )) as total_signed_contracts,
+    count(*) filter (where status = 'signature') as signed_contracts,
+    count(*) filter (
+        where status = 'construction permit'
+    ) as pending_license_response,
+    count(*) filter (where status = 'date installation set') as scheduled_works,
+    count(*) filter (where status = 'end installation') as works_completed,
+    count(*) filter (where status = 'legal registration') as legalization_stage,
+    count(*) filter (where status = 'legalization') as legalization_completed,
+    count(*) filter (where status in (
         'end installation',
         'legal registration',
         'legalization',
         'warranty payment'
-    )) AS total_executed_works,
-    COUNT(*) FILTER (WHERE status = 'warranty payment') AS warranty_payment_done,
-    AVG(
-        CASE
-            WHEN preregistration_date IS NOT NULL THEN ABS((preregistration_date - date_start_installation))
-            WHEN registration_date IS NOT NULL THEN ABS((registration_date - date_start_installation))
-            ELSE ABS((date_prereport - date_start_installation))
-        END
-    ) AS average_days_to_start_installation,
-    AVG(
-        CASE
-            WHEN preregistration_date IS NOT NULL THEN ABS((preregistration_date - date_legal_docs))
-            WHEN registration_date IS NOT NULL THEN ABS((registration_date - date_legal_docs))
-            ELSE ABS((date_prereport - date_legal_docs))
-        END
-    ) AS average_days_to_legalize_installation,
+    )) as total_executed_works,
+    count(*) filter (
+        where status = 'warranty payment'
+    ) as warranty_payment_done,
+    avg(
+        case
+            when
+                preregistration_date is not null
+                then abs((preregistration_date - date_start_installation))
+            when
+                registration_date is not null
+                then abs((registration_date - date_start_installation))
+            else abs((date_prereport - date_start_installation))
+        end
+    ) as average_days_to_start_installation,
+    avg(
+        case
+            when
+                preregistration_date is not null
+                then abs((preregistration_date - date_legal_docs))
+            when
+                registration_date is not null
+                then abs((registration_date - date_legal_docs))
+            else abs((date_prereport - date_legal_docs))
+        end
+    ) as average_days_to_legalize_installation,
     ((
-        (COUNT(*) FILTER (WHERE status IN ('end installation', 'legal registration', 'legalization', 'warranty payment'))::decimal +
-        COUNT(*) FILTER (WHERE status = 'discarded')::decimal) /
-        COUNT(*)::decimal) * 100) AS percentage_executed_and_discarded
-FROM {{ source('somsolet', 'somsolet_project')}}
--- WHERE campaign_id = {{ campaign_id }}
-GROUP BY campaign_id
+        (
+            count(*) filter (
+                where status in (
+                    'end installation',
+                    'legal registration',
+                    'legalization',
+                    'warranty payment'
+                )
+            )::decimal
+            + count(*) filter (where status = 'discarded')::decimal
+        )
+        / count(*)::decimal
+    ) * 100) as percentage_executed_and_discarded
+from {{ source('somsolet', 'somsolet_project') }}
+-- where campaign_id = {{ campaign_id }}
+group by campaign_id
